074926c2fe416b0f237141271f1a4e510a8b5855
george <nihaokexiao@gmail.com>
2025-11-19 20:18:24 +0800

    创建项目

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// 音频资源
+const sound_short_i = new Audio('./media/sound_short_i.mp3')
+
+// 获取元素
+
+const sound_short_i_btn = document.querySelectorAll('.contentIPA')
+sound_short_i_btn.addEventListener('click', (e) => {
+  console.log(e.target)
+  console.log(1233)
+})
